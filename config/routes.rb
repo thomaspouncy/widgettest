b6e709d01a2cd3561dacc9a250367affaad4fba8
thomaspouncy <thomas.pouncy@gmail.com>
2011-01-18 12:06:24 -0800

    testing routes

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Widgettest::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "index"
 
   # See how all your routes lay out with "rake routes"
 
