2baefa739669bab377a9944c7c265b3c84104386
Brian Jennings <brian.j.jennings@gmail.com>
2011-09-15 12:20:15 -0700

    Added some api testing methods

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,15 @@
 class WelcomeController < ApplicationController
+  require 'oauth2'
+  layout "application"
 
-	layout "application"
-	
+
+  def api_test
+    key = "39zuq9yiok6iaqek7b7zhy2vk"
+    secret = "79w7aplwms7xezs1lix4bnnxo"
+
+    client = OAuth2::Client.new(key, secret)
+
+    render :text => client
+
+  end
 end
